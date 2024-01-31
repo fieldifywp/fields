0d74523b43c2e9ef5d9f5cb26bfd8b3ad344604d
SEO Themes <seothemeswp@gmail.com>
2024-02-01 00:38:33 +0800

    js

--- public/js/index.asset.php
+++ public/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-server-side-render'), 'version' => 'e4e3e60414d73791d9c7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-server-side-render'), 'version' => '29a267af4ed2f82323c1');
