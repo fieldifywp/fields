649d6d1e26d0765bc1cb6c14e8b8d68ed6e37a04
SEO Themes <seothemeswp@gmail.com>
2024-02-03 01:00:55 +0800

    fix field ids

--- public/js/index.asset.php
+++ public/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-server-side-render'), 'version' => '455b84f14d2249fa4b8f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-server-side-render'), 'version' => '1aebe5e9a6d9f208e2b7');
