37ee81970c1284d14a8b33ccf864e77010d33df1
SEO Themes <seothemeswp@gmail.com>
2024-05-16 20:59:21 +0800

    Remove unnecessary checks

--- src/Fieldify.php
+++ src/Fieldify.php
@@ -12,54 +12,47 @@ use Fieldify\Fields\PostTypes;
 use Fieldify\Fields\Settings;
 use Fieldify\Fields\Taxonomies;
 
-if ( ! class_exists( 'Fieldify' ) ) {
+/**
+ * Fieldify facade.
+ *
+ * @since 1.0.0
+ */
+class Fieldify {
 
 	/**
-	 * Fieldify facade.
+	 * Services.
 	 *
-	 * @since 1.0.0
+	 * @var array
 	 */
-	class Fieldify {
+	private const SERVICES = [
+		Assets::class,
+		Blocks::class,
+		MetaBoxes::class,
+		PostTypes::class,
+		Settings::class,
+		Taxonomies::class,
+	];
 
-		/**
-		 * Services.
-		 *
-		 * @var array
-		 */
-		private const SERVICES = [
-			Assets::class,
-			Blocks::class,
-			MetaBoxes::class,
-			PostTypes::class,
-			Settings::class,
-			Taxonomies::class,
-		];
+	/**
+	 * Registers instance.
+	 *
+	 * @param string $file Main plugin or theme file.
+	 *
+	 * @return void
+	 */
+	public static function register( string $file ): void {
+		static $container = null;
 
-		/**
-		 * Registers instance.
-		 *
-		 * @param string $file Main plugin or theme file.
-		 *
-		 * @return void
-		 */
-		public static function register( string $file ): void {
-			static $container = null;
+		if ( ! is_null( $container ) || ! file_exists( $file ) ) {
+			return;
+		}
 
-			if ( ! is_null( $container ) || ! file_exists( $file ) ) {
-				return;
-			}
+		$container = new Container();
 
-			$container = new Container();
+		$container->make( Config::class, $file );
 
-			$container->make( Config::class, $file );
-
-			foreach ( self::SERVICES as $id ) {
-				$service = $container->make( $id );
-
-				if ( is_object( $service ) ) {
-					Hook::annotations( $service );
-				}
-			}
+		foreach ( self::SERVICES as $id ) {
+			Hook::annotations( $container->make( $id ) );
 		}
 	}
 }
